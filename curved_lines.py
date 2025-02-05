f2553a8e80cfa640d396eb20b9722f5452bc12f7
1002215 <149686629+1002215@users.noreply.github.com>
2025-02-05 08:45:41 -0500

    Update curved_lines.py

--- curved_lines.py
+++ curved_lines.py
@@ -9,7 +9,7 @@ def detect(image):
     # some preprocessing
     thin = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     cv2.imshow("gray", thin)
-    thin = cv2.blur(thin, (10, 10))
+    thin = cv2.blur(thin, (20, 20))
     cv2.imshow("blur", thin)
     _, thin = cv2.threshold(thin, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     cv2.imshow("threshold", thin)
@@ -24,26 +24,58 @@ def detect(image):
 
 
     # https://stackoverflow.com/questions/19222343/filling-contours-with-opencv-python
-    contours, hierarchy = cv2.findContours(thin, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-    print(len(contours))
-    cv2.drawContours(image, contours, 0, (0,0,255), thickness= 1)
-    cv2.drawContours(image, contours, 1, (0,0,255), thickness = 1)
+    contours, hierarchy = cv2.findContours(thin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.drawContours(image, contours, 2, (0,0,255), thickness= 10)
+    cv2.drawContours(image, contours , 1, (0,0,255), thickness = 10)
 
     # thin image to find clear contours
     thin = cv2.ximgproc.thinning(thin, thinningType=cv2.ximgproc.THINNING_GUOHALL)
     cv2.imshow("thin", thin)
 
     # dind contours
-    cnts, _ = cv2.findContours(thin, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    cnts, _ = cv2.findContours(thin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     # c = max(cnts, key=cv2.contourArea)
 
     cv2.drawContours(image, cnts, 1, (36, 255, 12), 2)
     #image = cv2.polylines(final, [vertices], True, (0,0,255), 1)
-    return image
+    rows, columns = image.shape[0], image.shape[1]
+    cropped = image[100:rows-100, 100:columns-100]
+    return cropped
 
-image = cv2.imread("Screenshot 2025-02-04 at 8.18.02 AM.png")
-cv2.imshow("final", detect(image))
-cv2.waitKey(0)
+cap = cv2.VideoCapture(0)  # 0 for default camera, or provide a video file path
+
+if cap.isOpened():
+
+    while True:
+
+        try:
+
+            # Read the frame from the video capture.
+            success, frame = cap.read()
+
+            # If the frame was not read correctly, break the loop to end the program.
+            if success == True:
+
+                # Display the frame.
+                cv2.imshow("Emma Chetan Circumference and Center Video PWP", detect(frame))
+
+                # Check if the user pressed 'q' to quit.
+                if cv2.waitKey(25) == ord('q'):
+
+                    break
+
+            else:
+
+                break
+
+        except Exception:
+
+            continue
+
+# Release the video capture and close all windows.
+cap.release()
+
 cv2.destroyAllWindows()
